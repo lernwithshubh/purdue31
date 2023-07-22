8134c7d6b54cb6bcccf509986a93fa560f0acbc0
gituser <git@ip-172-31-82-50.ec2.internal>
2023-07-22 16:33:31 +0000

    modified 1.java

--- 1.java
+++ 1.java
@@ -1,7 +1,7 @@
 This is from feature branch
 This is for testing conflicts.
 This is from defect branch.
-
+Checking for ssh connectivity
 testing rebase operation from defect branch
 testing rebase from feature branch
 This is from vs code. 
